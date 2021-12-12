ee96bfa690bf213315035162d2c717cf09f2cd23
Ryu-ga <rladydgk05679@gmail.com>
2021-12-12 09:25:23 +0900

    Initial Commit

---
+++ WebtoonViewer/WebtoonViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebtoonViewerApp.swift
+//  WebtoonViewer
+//
+//  Created by Kim Yong Ha on 2021/12/12.
+//
+
+import SwiftUI
+
+@main
+struct WebtoonViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
